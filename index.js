0aadf13fe26044f945ad10dadad65ffa26968c2f
mkilic <mkilic.ktu@gmail.com>
2021-03-28 22:43:11 +0300

    Version-3

---
+++ index.js
@@ -0,0 +1,6 @@
+var a = "fatih";
+var person = {name : "Faith", surname : "Tuncer", age : 30 }
+var persons = [
+	{name : "Faith", surname : "Tuncer", age : 30 },
+	{name : "Mehmet", surname : "Kilic", age : 33 }
+]
