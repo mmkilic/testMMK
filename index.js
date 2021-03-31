e574a485e63135d141a2bc998d16e16d2d16a929
MuratGURCEMAL <muratgurcemal@gmail.com>
2021-03-31 09:28:15 +0300

    01

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var persons = [
 	{ name: "Faith", surname: "Tuncer", age: 30 },
 	{ name: "Mehmet", surname: "Kilic", age: 33 },
 	{ name: "Murat", surname: "GÜRCEMAL", age: 30 },
-	{ name: "TEST", surname: "002", age: 30 },
+	{ name: "TEST 3", surname: "002", age: 30 },
 
 ]
