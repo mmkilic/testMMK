4e12c63ec81ca2b67d605d00a52c972bfb2a07e5
Murat GÜRCEMAL <muratgurcemal@gmail.com>
2021-03-31 09:24:04 +0300

    Update index.js
    github.com

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var person = {name : "Faith", surname : "Tuncer", age : 30 }
 var persons = [
 	{ name: "Faith", surname: "Tuncer", age: 30 },
 	{ name: "Mehmet", surname: "Kilic", age: 33 },
-	{ name: "Murat", surname: "GÜRCEMAL", age: 30 },
+	{ name: "Murat", surname: "GÃœRCEMAL", age: 30 },
 	{ name: "TEST", surname: "002", age: 30 },
+	{ name: "TEST 2", surname: "002", age: 30 },
 
 ]
